f39e28e6a0b68bbf7be85d767b27b744f0f5299b
kakuilan <kakuilan@163.com>
2019-09-12 14:31:40 +0800

    使用接口

---
+++ src/t025.ts
@@ -0,0 +1,17 @@
+// 使用接口,来确保类拥有指定的解构
+interface LoggerInterface {
+  log(arg: any) : void;
+}
+
+class Logger implements LoggerInterface {
+  log(arg) {
+    if (typeof console.log ==="function") {
+      console.log(arg);
+    }else{
+      alert(arg);
+    }
+  }
+}
+
+var obj = new Logger()
+obj.log("heheh")
