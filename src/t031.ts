371249fd102c571b4c566150db77b601e0ed9f34
kakuilan <kakuilan@163.com>
2019-09-13 15:06:49 +0800

    类的私有变量

--- src/t031.ts
+++ src/t031.ts
@@ -2,6 +2,7 @@
 
 var bar = 0; //全局
 (function (global) {
+  console.log('global', global);
   var foo : number = 0; //在函数作用域中
   bar = 1; //在全局作用域中
   console.log(global.bar);
---
+++ src/t032.ts
@@ -0,0 +1,29 @@
+// 类的私有变量
+class Counter {
+  private _i : number;
+
+  constructor() {
+    this._i = 0;
+  }
+
+  get() : number {
+    return this._i;
+  }
+
+  set(val:number):void{
+    this._i = val;
+  }
+
+  increment():void{
+    this._i++;
+  }
+
+}
+
+var counter = new Counter();
+console.log(counter.get());
+counter.set(2);
+console.log(counter.get());
+counter.increment();
+console.log(counter.get());
+//console.log(counter._i); //error
