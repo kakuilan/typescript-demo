70de881823442baab9e2f9587d8b48632016892f
kakuilan <kakuilan@163.com>
2019-09-13 14:55:45 +0800

    立即调用函数

---
+++ src/t031.ts
@@ -0,0 +1,14 @@
+// 立即调用函数
+
+var bar = 0; //全局
+(function (global) {
+  var foo : number = 0; //在函数作用域中
+  bar = 1; //在全局作用域中
+  console.log(global.bar);
+  console.log(foo);
+})(this);
+
+console.log(bar);
+//console.log(foo); //error
+
+export {}
