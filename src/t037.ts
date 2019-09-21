66ed6f03ae0c3f322bff294cf163b53be96cb2b7
kakuilan <kakuilan@163.com>
2019-09-21 10:59:34 +0800

    add 异步函数,async和await

---
+++ src/t037.ts
@@ -0,0 +1,15 @@
+// 异步函数,async和await
+
+function timeout(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function asyncPrint(value, ms) {
+  await timeout(ms);
+  console.log(value);
+}
+
+asyncPrint('hello world', 250);
+
