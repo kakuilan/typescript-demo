c9452d31349a197be8cbafd7945934b2321d9c39
kakuilan <kakuilan@163.com>
2021-08-04 18:45:47 +0800

    接口-类类型

---
+++ src/t007.ts
@@ -0,0 +1,18 @@
+//接口-类类型
+
+interface ClockInterface {
+    currentTime: Date;
+
+    setTime(d: Date): void;
+}
+
+class Clock implements ClockInterface {
+    currentTime: Date = new Date()
+
+    setTime(d: Date) {
+        this.currentTime = d;
+    }
+
+    constructor(h: number, m: number) {
+    }
+}
